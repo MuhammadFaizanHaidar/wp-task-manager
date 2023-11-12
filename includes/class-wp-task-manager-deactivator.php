3860ad05d1c69bf34c0b16e8aa062f05b8cc915f
faizanhaidar <faizanhaider594@gmail.com>
2023-11-12 20:18:30 +0500

    added basic structure

---
+++ includes/class-wp-task-manager-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://faizanhaidar.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Task_Manager
+ * @subpackage Wp_Task_Manager/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Task_Manager
+ * @subpackage Wp_Task_Manager/includes
+ * @author     Muhammad Faizan Haidar <faizanhaider594@gmail.com>
+ */
+class Wp_Task_Manager_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
